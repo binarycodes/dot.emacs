fd021f904d214f6d38f5212fdde9cd0f5f0efde7
sujoy <sujoy@archlinux.us>
2010-10-11 22:17:04 +0530

    added key bindings for indenting regions and calling man directly

--- my-input.el
+++ my-input.el
@@ -1,8 +1,9 @@
 (provide 'my-input)
 
 ;; key bindings
+(global-set-key [f1] 'man)
 (global-set-key [f5] 'slime)
 (global-set-key [f6] 'slime-selector)
 (global-set-key [f9] 'myerc-start-or-switch)
 (global-set-key (kbd "M-/") 'hippie-expand)
-
+(global-set-key [(control tab)] 'indent-region)
