8c92f57d9f234745e786ef4e3b2fcd430e82fd57
Sujoy Kumar Das <sujoy@archlinux.us>
2011-05-07 12:59:13 +0530

    use magit more than slime these days hence the change

--- my-input.el
+++ my-input.el
@@ -3,9 +3,8 @@
 ;; key bindings
 (global-set-key [f1] 'man)
 (global-set-key [f5] 'slime)
-(global-set-key [f6] 'slime-selector)
+(global-set-key [f6] 'magit-status)
 (global-set-key [f9] 'myerc-start-or-switch)
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key [(control tab)] 'indent-region)
 (global-set-key (kbd "<f12>") 'my-kill-non-file-buffs)
-;;(global-set-key (kbd "<f12>") 'whitespace-cleanup)
