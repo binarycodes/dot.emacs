d8793014d6c7a41ff857c0e67a45a23c325ca332
Sujoy Kumar Das <sujoy@archlinux.us>
2011-09-04 13:37:53 +0530

    custom file is already loaded so no point adding it to load list

--- init.el
+++ init.el
@@ -14,10 +14,9 @@
 ;; add paths to the load-path
 (add-to-load-path-with-subdirs (expand-file-name "~/.emacs.d")
 							   '(".." "." ".srecode" "auto-save-list"
-								 "history" "my-backup-files" "eshell")
+								 "history" "my-backup-files" "eshell" "custom.el")
 							   '("plugins" "my-lisp"))
 
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/cedet/common")
 
 (load-file (expand-file-name "~/.emacs.d/my-requires.el"))
-
