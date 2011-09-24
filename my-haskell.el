ddb710e675609be9a51763a4e9137d8c0fadd4cc
Sujoy Kumar Das <sujoy@archlinux.us>
2011-09-24 10:41:07 +0530

    setup for inferior haskell

--- my-haskell.el
+++ my-haskell.el
@@ -1,9 +1,12 @@
 (provide 'my-haskell)
 
+(require 'inf-haskell)
+
 (setq auto-mode-alist
       (append auto-mode-alist
               '(("\\.[hg]s$"  . haskell-mode)
                 ("\\.hi$"     . haskell-mode)
-                ("\\.l[hg]s$" . literate-haskell-mode))))
+                ("\\.l[hg]s$" . literate-haskell-mode)))
+	  haskell-program-name "ghci")
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
