9ad0cc4ac836f7952d3e3997b46c7f3098f137c5
Sujoy Kumar Das <sujoy@archlinux.us>
2012-01-14 15:47:16 +0530

    using dired-omit-mode to omit dotfiles from listing

--- my-dired.el
+++ my-dired.el
@@ -8,10 +8,13 @@ it's loaded."
   (require 'dired-single)
   (require 'dired-x)
 
+  (dired-omit-mode 1)
+
+  ;; key bindings for dired-mode
   (define-key dired-mode-map [return] 'dired-single-buffer)
   (define-key dired-mode-map [down-mouse-1] 'dired-single-buffer-mouse)
   (define-key dired-mode-map "W" 'wdired-change-to-wdired-mode)
-  (define-key dired-mode-map "^"
-    (function
-     (lambda nil (interactive) (dired-single-buffer ".."))))
-  (setq dired-listing-switches "-alh"))
+  (define-key dired-mode-map "^" '(lambda () (interactive) (dired-single-buffer "..")))
+
+  (setq dired-listing-switches "-alh"
+        dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\..*$"))
