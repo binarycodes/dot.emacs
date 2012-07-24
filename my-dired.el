71bfc05c8e6c978deda5143771f8eb40de41370f
Sujoy Kumar Das <sujoy@archlinux.us>
2012-07-25 01:01:23 +0530

    dired+ included for a colorful experience. enabling recursive directory operations.

--- my-dired.el
+++ my-dired.el
@@ -10,20 +10,25 @@
 (defun my-dired-init ()
   "Bunch of stuff to run for dired, either immediately or when
 it's loaded."
-  (require 'dired-single)
-  (require 'dired-x)
 
-  (dired-omit-mode 1)
+  (require 'dired-single)
+  (require 'dired-x)                    ;for colorful listing
+
+  (add-hook 'dired-mode-hook (lambda () (dired-omit-mode 1)))
 
   ;; key bindings for dired-mode
-  (define-key dired-mode-map [return] 'dired-single-buffer)
-  (define-key dired-mode-map [down-mouse-1] 'dired-single-buffer-mouse)
+  (define-key dired-mode-map (kbd "RET") 'dired-single-buffer)
+  (define-key dired-mode-map (kbd "<down-mouse-1>") 'dired-single-buffer-mouse)
   (define-key dired-mode-map (kbd "W") 'wdired-change-to-wdired-mode)
   (define-key dired-mode-map (kbd "^") '(lambda () (interactive) (dired-single-buffer "..")))
   (define-key dired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
 
   (setq dired-listing-switches "-alh"
-        dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\..*$"))
+        dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\..*$")
+
+  ;; allow recursive directory opertions
+  (setq dired-recursive-copies 'always)
+  (setq dired-recursive-deletes 'top))
 
 
 (eval-after-load "wdired"
@@ -31,3 +36,6 @@ it's loaded."
      (define-key wdired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
      (define-key wdired-mode-map (kbd "M-<up>") 'dired-back-to-top)
      (define-key wdired-mode-map (kbd "M-<down>") 'dired-jump-to-bottom)))
+
+
+(require 'dired+)
